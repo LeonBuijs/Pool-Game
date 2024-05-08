b84b25a5fb56a10d3ba558276488bc23e89bc6df
Jahper <jaspertr04@gmail.com>
2024-05-08 14:15:52 +0200

    code opgeschoond

--- src/Ball.java
+++ src/Ball.java
@@ -1,9 +1,5 @@
 import org.dyn4j.dynamics.Body;
-import org.dyn4j.geometry.MassType;
 import org.dyn4j.geometry.Vector2;
-import org.jfree.fx.FXGraphics2D;
-
-import java.awt.image.BufferedImage;
 
 public class Ball {
     private BallType ballType;
@@ -20,14 +16,11 @@ public class Ball {
     public void update() {
         Vector2 vector2 = ball.getLinearVelocity();
         if (!running) {
-            ball.applyForce(new Vector2(0,0));
+            ball.applyForce(new Vector2(0, 0));
             running = true;
         } else {
             ball.applyImpulse(new Vector2(-vector2.x * 0.2, -vector2.y * 0.2));
         }
-
-//          todo dit als checker te gebruiken om te kijken of alle ballen stil liggen.
-//        if (vector2.x < -1.0e-3 || vector2.x > 1.0e-3 && vector2.y < -1.0e-3 || vector2.y> 1.0e-3) {
 
 //        test sout
 //        if (ballType.equals(BallType.WHITE)) {
@@ -35,10 +28,9 @@ public class Ball {
 //        }
     }
 
-    public boolean checkRolling(){
+    public boolean checkRolling() {
         Vector2 vector2 = ball.getChangeInPosition();
         if (vector2.x < 1.0e-3 && vector2.y < 1.0e-3) {
-//            System.out.println("test");
             return false;
         }
         return true;
